c2a9e16338ad97e2b222044d481fc973efc6e733
wifiextender <sayers.uk>
2013-11-11 19:05:28 +0200

    pdf2img

--- pdf2img_gui.py
+++ pdf2img_gui.py
@@ -6,6 +6,17 @@ from gi.repository import Gtk, Gdk
 
 class MainWindow(Gtk.Window):
 
+    def about_dialog(self, widget):
+        aboutdialog = Gtk.AboutDialog()
+        aboutdialog.set_name("About")
+        aboutdialog.set_version("v0.2")
+        aboutdialog.set_comments("Convert PDF book to multiple images\nin various formats with a single mouse click")
+        aboutdialog.set_website("http://linux.sytes.net/")
+        aboutdialog.set_website_label("Developer Website")
+        aboutdialog.set_authors(["Aaron Caffrey"])
+        aboutdialog.run()
+        aboutdialog.destroy()
+
     #def comboboxtext_changed(self, comboboxtext):
             #comboboxtext.get_active_text()
 
@@ -55,14 +66,26 @@ class MainWindow(Gtk.Window):
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=8)
         self.add(vbox)
 
+        grid = Gtk.Grid()
+        grid.set_row_spacing(5)
+        grid.set_column_spacing(5)
+        vbox.add(grid)
+
         label = Gtk.Label(label="Resolution Number")
-        vbox.pack_start(label, False, False, 0)
+        grid.attach(label, Gtk.PositionType.LEFT, 1, 1, 1)
 
         self.entry = Gtk.Entry()
         self.entry.set_text("100")
         self.entry.set_max_length(4)
-        vbox.pack_start(self.entry, True, True, 0)
+        grid.attach(self.entry, Gtk.PositionType.LEFT, 2, 1, 1)
 
+        label = Gtk.Label(label="About")
+        grid.attach(label, Gtk.PositionType.RIGHT, 1, 1, 1)
+
+        self.button_about = Gtk.ToolButton(stock_id=Gtk.STOCK_ABOUT)
+        self.button_about.connect("clicked", self.about_dialog)
+        #self.button1.connect("clicked", reveal_child)
+        grid.attach(self.button_about, Gtk.PositionType.RIGHT, 2, 1, 1)
         #grid = Gtk.Grid()
         #vbox.add(grid)
 
@@ -81,30 +104,31 @@ class MainWindow(Gtk.Window):
         #button = Gtk.Button("Reveal")
         #button.connect("clicked", reveal_child)
         #grid.attach(button, 0, 1, 1, 1)
-        label = Gtk.Label(label="From page number:")
-        vbox.add(label)
+
+        label = Gtk.Label(label="From page:")
+        grid.attach(label, Gtk.PositionType.LEFT, 3, 1, 1)
         adjustment = Gtk.Adjustment(value=1, lower=1, upper=9999, step_increment=1)
         self.spinbutton = Gtk.SpinButton(adjustment=adjustment, climb_rate=1, digits=0)
-        vbox.add(self.spinbutton)
+        grid.attach(self.spinbutton, Gtk.PositionType.LEFT, 4, 1, 1)
 
         label = Gtk.Label(label="To:")
-        vbox.add(label)
+        grid.attach(label, Gtk.PositionType.RIGHT, 3, 1, 1)
         adjustment = Gtk.Adjustment(value=1, lower=1, upper=9999, step_increment=1)
         self.spinbutton2 = Gtk.SpinButton(adjustment=adjustment, climb_rate=1, digits=0)
-        vbox.add(self.spinbutton2)
+        grid.attach(self.spinbutton2, Gtk.PositionType.RIGHT, 4, 1, 1)
 
-        label = Gtk.Label(label="Image extension")
-        vbox.add(label)
+        label = Gtk.Label(label="Image format")
+        grid.attach(label, Gtk.PositionType.LEFT, 5, 1, 1)
         self.comboboxtext2 = Gtk.ComboBoxText()
         self.comboboxtext2.append("png", "png")
         self.comboboxtext2.append("jpg", "jpg")
         self.comboboxtext2.append("bmp", "bmp")
         self.comboboxtext2.append("tiff", "tiff")
         self.comboboxtext2.set_active(0)
-        vbox.add(self.comboboxtext2)
+        grid.attach(self.comboboxtext2, Gtk.PositionType.LEFT, 6, 1, 1)
 
         label = Gtk.Label(label="sDevice")
-        vbox.add(label)
+        grid.attach(label, Gtk.PositionType.RIGHT, 5, 1, 1)
         self.comboboxtext = Gtk.ComboBoxText()
         self.comboboxtext.append("png16m", "png16m")
         self.comboboxtext.append("pngalpha", "pngalpha")
@@ -118,7 +142,7 @@ class MainWindow(Gtk.Window):
         self.comboboxtext.append("tiffgray", "tiffgray")
         self.comboboxtext.set_active(0)
         #self.comboboxtext.connect("changed", self.comboboxtext_changed)
-        vbox.add(self.comboboxtext)
+        grid.attach(self.comboboxtext, Gtk.PositionType.RIGHT, 6, 1, 1)
 
         label = Gtk.Label(label="Select PDF file")
         vbox.add(label)
